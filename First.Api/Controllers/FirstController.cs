3c5f1696ee699194a61a98426c8525c2a1b36ebc
Omar Faruk Munna <omarmunna67@outlook.com>
2020-08-29 17:10:14 +0600

    Add project files.

---
+++ First.Api/Controllers/FirstController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First.Api.Controllers
+{
+    [Route("api/first")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok();
+        }
+
+    }
+}
